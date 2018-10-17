8dc85241d961f5828140412c160cb485286e41f5
sivashanmugam-kannan <sivashanmugam.kannan@funtoot.com>
2018-10-17 14:16:19 +0530

    logic change to find affected questions

--- find_mtf_v2_affected_questions.js
+++ find_mtf_v2_affected_questions.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 var _ = require('lodash');
-fs.readdir('./affected_questions', (err, files) => {
+fs.readdir('./all_questions', (err, files) => {
     _.each(files, function(file){
-        fs.readFile('./affected_questions/' + file, function(err, data){
+        fs.readFile('./all_questions/' + file, function(err, data){
             if(err){
                 throw err;
             }
@@ -10,7 +10,43 @@ fs.readdir('./affected_questions', (err, files) => {
             var body = JSON.parse(data.result.assessment_item.body);
             var lhs = body.data.data.option.optionsLHS;
             var rhs = body.data.data.option.optionsRHS;
+            var question = body.data.data.question;
+            var mediaCount = 0;
+            var expectedMediaCount = body.data.media.length;
             var affected = false;
+
+            if(question.image.length != 0){
+                mediaCount++
+            }
+
+            if(question.audio.length != 0){
+                mediaCount++
+            }
+
+            for(var i = 0;i < lhs.length;i++){
+                if(lhs[i].image.length != 0){
+                    mediaCount++
+                }
+
+                if(lhs[i].audio.length != 0){
+                    mediaCount++
+                }
+
+                if(rhs[i].image.length != 0){
+                    mediaCount++
+                }
+
+                if(rhs[i].audio.length != 0){
+                    mediaCount++
+                }
+            }
+
+            if(mediaCount != expectedMediaCount){
+                affected = true;
+            }
+            
+            /*
+            OLD login
             for(var i = 0;i < lhs.length;i++){
                 if(lhs[i].image.length != 0 && rhs[i].image.length != 0){
                     affected = true;
@@ -19,12 +55,11 @@ fs.readdir('./affected_questions', (err, files) => {
                     affected = true;
                 }
             }
+            */
+
             if(affected){
                 console.log( file + ' AFFECTED');
-            } else{
             }
-            
-        
         })        
     })
     
